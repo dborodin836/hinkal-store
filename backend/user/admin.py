5c2e29ba56c7d19dd5092f732befa70d033c0a0f
dborodin836 <dborodin836@gmail.com>
2022-03-22 20:51:11 +0200

    FEAT: Backend: added tables for user's address

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import CustomUser
+
+from .models import CustomUser, Countries, UserAddress
+
+
+class CountriesAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserAddressAdmin(admin.ModelAdmin):
+    pass
 
 
 admin.site.register(CustomUser)
+admin.site.register(Countries, CountriesAdmin)
+admin.site.register(UserAddress, UserAddressAdmin)
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,10 +1,34 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 from django.conf import settings
 
 
+class Countries(models.Model):
+    """Table for countries and their codes"""
+    fullname = models.CharField(max_length=100)
+    code = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.fullname
+
+
+class UserAddress(models.Model):
+    """Contains user's addresses"""
+    user_id = models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    address_1 = models.CharField(max_length=255)
+    address_2 = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=50)
+    postal_code = models.PositiveIntegerField()
+    county = models.ForeignKey(Countries, on_delete=models.SET_NULL, null=True)
+    telephone = models.CharField(max_length=30)
+    mobile = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.user_id.username
+
+
 class CustomUser(AbstractUser):
+    """Custom user model with some extra fields"""
     image = models.ImageField(upload_to='photos/%Y/%m/%d/', default=settings.DEFAULT_USER_PHOTO)
     is_vendor = models.BooleanField(default=False)
     biography = models.TextField(blank=True, null=True)
