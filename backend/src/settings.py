34b58608acda0f039b79cee76ea4573130451ea9
PootisMaan <dborodin836@gmail.com>
2022-05-07 21:16:04 +0300

    WIP: Backend: moved func 'get_envs_from_file' to manage.py

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -3,32 +3,6 @@ from datetime import timedelta
 from pathlib import Path
 
 
-def get_envs_from_file(env_file="./local.env") -> None:
-    """
-    Open file and set the all env variables it contains.
-
-    Example:
-
-    # "./local.env" #
-        ...
-        # DB settings        -- will be ignored
-        DB_HOST=foobar       -- will be set as "DB_HOST" environment variable with value "foobar"
-        DB_PASS=span_eggs       os.environ["DB_HOST"] = "foobar"
-        ...
-    """
-    with open(env_file) as env_file:
-        for line in env_file:
-            if line.strip().startswith("#"):
-                continue
-            try:
-                key, value = line.strip().split("=", 1)
-                if isinstance(value, str):
-                    os.environ[key] = value
-            # In case some comments exist in file
-            except ValueError:
-                continue
-
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
