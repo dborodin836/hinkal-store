e10f10b31a8a951c0833ab7ab848e2a28eb816ee
PootisMaan <dborodin836@gmail.com>
2022-05-10 17:28:11 +0300

    WIP: Backend: fixed Docker, added wait_for_db command

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -70,10 +70,10 @@ WSGI_APPLICATION = "src.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USERNAME"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
     }
 }
 
