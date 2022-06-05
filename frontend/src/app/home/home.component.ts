2996fcab3cd43e5b746b99733c29d336009d632c
dborodin836 <dborodin836@gmail.com>
2022-06-05 17:50:43 +0300

    WIP: Frontend: added home hero

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import {HttpResponse} from "@angular/common/http";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
