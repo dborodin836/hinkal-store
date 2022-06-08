2e4555c9d0809b571c53418a52a1903ce55f1d6f
dborodin836 <dborodin836@gmail.com>
2022-06-08 19:25:26 +0300

    REFACTOR: Backend: black refactor

--- backend/src/apiurls.py
+++ backend/src/apiurls.py
@@ -11,8 +11,6 @@ router.register(r"contact", ContactViewSet)
 router.register(r"order", OrderViewSet)
 router.register(r"order-item", OrderItemViewSet)
 router.register(r"discount", DiscountViewSet)
-router.register(
-    r"best-selling-dishes", BestSellingDishesViewSet, basename="best-selling-dishes"
-)
+router.register(r"best-selling-dishes", BestSellingDishesViewSet, basename="best-selling-dishes")
 
 urlpatterns = [path(r"", include(router.urls))]
