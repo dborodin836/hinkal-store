ce3db7318c52a85b640f854a9e348f46540cb48e
dborodin836 <dborodin836@gmail.com>
2022-06-22 21:12:49 +0300

    FIX: Backend: fixed non running tests

--- backend/manage.py
+++ backend/manage.py
@@ -10,7 +10,10 @@ def main():
     """Run administrative tasks."""
     sys.path.append(str(Path(__file__).resolve().parent.parent))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
-    get_envs_from_file('docker.env')
+    if "test" in sys.argv or "test\_coverage" in sys.argv:
+        get_envs_from_file("test.env")
+    else:
+        get_envs_from_file("docker.env")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
