eb41f49dbeec55e8c3bddcc367b6e7f92cc1d4ce
dborodin836 <dborodin836@gmail.com>
2022-07-19 12:46:19 +0300

    REFACTOR: Frontend: refactor frontend

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,62 +1,64 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder} from "@angular/forms";
-import {LoginService} from "../services/login.service";
-import {Router} from "@angular/router";
-import {MatSnackBar} from "@angular/material/snack-bar";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { LoginService } from '../services/login.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor(private formBuilder: FormBuilder,
-              private loginService: LoginService,
-              private router: Router,
-              private snackBar: MatSnackBar) {
-  }
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   registerForm = this.formBuilder.group({
     username: '',
     email: '',
     password: '',
     password_repeat: '',
-  })
+  });
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit() {
-    if (this.registerForm.value.username.length === 0){
-      this.snackBar.open("Enter username.", "X", {
+    if (this.registerForm.value.username.length === 0) {
+      this.snackBar.open('Enter username.', 'X', {
         duration: 7000,
-        horizontalPosition: "end"
-      })
-      return
+        horizontalPosition: 'end',
+      });
+      return;
     }
     if (this.registerForm.value.email.length === 0) {
-      this.snackBar.open("Enter Email.", "X", {
+      this.snackBar.open('Enter Email.', 'X', {
         duration: 7000,
-        horizontalPosition: "end"
-      })
-      return
+        horizontalPosition: 'end',
+      });
+      return;
     }
-    if (this.registerForm.value.password.length === 0){
-      this.snackBar.open("Enter Password.", "X", {
+    if (this.registerForm.value.password.length === 0) {
+      this.snackBar.open('Enter Password.', 'X', {
         duration: 7000,
-        horizontalPosition: "end"
-      })
-      return
+        horizontalPosition: 'end',
+      });
+      return;
     }
     if (this.registerForm.value.password === this.registerForm.value.password_repeat) {
-      this.loginService.register(this.registerForm.value.username, this.registerForm.value.password, this.registerForm.value.email)
+      this.loginService.register(
+        this.registerForm.value.username,
+        this.registerForm.value.password,
+        this.registerForm.value.email
+      );
     } else {
-      this.snackBar.open("Password do not match.", "X", {
+      this.snackBar.open('Password do not match.', 'X', {
         duration: 7000,
-        horizontalPosition: "end"
-      })
+        horizontalPosition: 'end',
+      });
     }
   }
-
 }
